b7238b350e1e6d186587697952732865c6afad22
Nisal Sanjula <nisalsanjula355@gmail.com>
2026-08-02 23:25:46 +0530

    modified the variables

--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "aws_region" {
   type = string
   description = "region where AWS resources are created by default"
+  default = "us-east-1"
 }
 
 variable "aws_ami_id" {
   type = string
   description = "AMI id of the AWS instance"
+  default = "ami-0e8a34246278c21e4"
 }
 
 variable "aws_instance_count" {
   type = number
   description = "AWS EC2 instance count"
+  default = 1
 }
 
 variable "aws_instance_type" {
   type = string
   description = "AWS instance type"
+  default = "t3.micro"
 }
